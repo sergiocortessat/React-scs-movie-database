dd9dc89e39c7bf61041074eed7a0bda66642da41
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-18 12:12:17 +0200

    Add new changes to movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// Rotuing
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Components
 import Header from './components/Header';
 import Home from './components/Home';
