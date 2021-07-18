53a931e13a77368cdd3588fcae523238d5d1b85a
Sergio Cortes Satizabal <sergiocortessat@gmail.com>
2021-07-18 15:05:29 +0200

    Add finished styles actors

--- src/components/Actor/Actor.style.js
+++ src/components/Actor/Actor.style.js
@@ -4,6 +4,26 @@
 /* eslint-disable no-unused-vars */
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  color: var(--white);
+  background: var(--darkGrey);
+  border-radius: 20px;
+  padding: 5px;
+  text-align: center;
 
-export const Image = styled.img``;
+  h3 {
+    margin: 10px 0 0 0;
+  }
+
+  p {
+    margin: 5px;
+  }
+`;
+
+export const Image = styled.img`
+    display: block;
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 15px;
+`;
